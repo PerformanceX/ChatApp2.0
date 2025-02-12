7b1c99302a97cf0ad7a3375b364d245b1070fa51
PerformanceX <performancex271@gmail.com>
2025-02-12 18:49:01 +0100

    Add files via upload
    ui update

--- app.py
+++ app.py
@@ -7,7 +7,31 @@ def main():
     load_dotenv()
     st.set_page_config(page_title="PerformanceX")
     st.title("PerformanceX")
-    st.header("Your Personalized AI Coach", divider="blue")
+    #st.header("A pioneering generative AI startup from Sweden, offers a unique Personal Co-Pilot for athletes. , our AI-driven platform helps athletes reduce stress and achieve their full potential. We prioritize data security and ensure our users' information is protected with robust security measures", divider="blue")
+    st.subheader("A pioneering generative AI startup from Sweden, offers a unique Personal Co-Pilot for athletes", divider=True)
+    
+    multi = '''
+    <p style="text-align: justify;">
+    Combining personalized coaching, 
+    psychological support, and performance optimization, our AI-driven platform helps athletes reduce stress and achieve their full potential. 
+    We prioritize data security and ensure our users' information is protected with robust security measures.
+    </p>
+    '''
+
+    st.markdown(multi, unsafe_allow_html=True)
+    
+    questions = '''
+    <h4 style="text-align: justify;">Here are three engaging questions to kick off a conversation with our Personal Sports Coach:</h3>
+    <ol style="text-align: justify;">
+        <li>What's your biggest challenge in your sport right now?</li>
+        <li>How do you typically prepare mentally before a big game or competition?</li>
+        <li>What aspect of your training or performance do you wish you had more support with?</li>
+    </ol>
+    '''
+
+    st.markdown(questions, unsafe_allow_html=True)
+
+
     st.sidebar.image("https://dcassetcdn.com/design_img/4077411/1363205/33612364/n1fphb57h65khkmdxps3m2h6b3_image.png")
 
     embeddings = create_embeddings()
